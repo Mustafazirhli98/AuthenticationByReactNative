b78c3c75f5c5492e73e269a07f50e634cc910184
Mustafa Zırhlı <mustafazirhli98@gmail.com>
2024-05-09 22:42:08 +0300

    errorMessage Fix

--- constants/ErrorMesages.js
+++ constants/ErrorMesages.js
@@ -1,9 +1,9 @@
 export const errorMessages = {
     signUpErrors: {
-        emailError: { title: "invalid email", body: "First of all you must enter a valid email adress!" },
+        emailError: { title: "Invalid email", body: "First of all you must enter a valid email adress!" },
         passwordError: { title: "invalid password", body: "Enter a password which is greater than 6 and less than 11" },
         confirmPasswordError: { title: "warning!", body: "Passwords are not matched!" }
     },
-    loginError: { title: "invalid user!", body: "Check your Credentials" },
+    loginError: { title: "Invalid user!", body: "Check your Credentials" },
     httpError: { title: "OOPPS!", body: "Can not connect now, please try again later" }
 }
